e66fb42464574d595c7f33dfa3d60b36ee73444d
Sebastian <s3v4s61@gmail.com>
2021-03-22 23:09:47 -0600

    La conexion a la base de datos

---
+++ database.php
@@ -0,0 +1,10 @@
+<$php
+    $conexion = mysqli_connect(
+        'localhost',  #Aqui sera la direccion de la base de datos
+        'root',  #Usuario de la base de datos
+        '',   #Password
+        'electronicos'  #Nombre de la base de datos 
+    );
+
+
+?>
