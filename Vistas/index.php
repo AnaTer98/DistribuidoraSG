67e7cb9ef22043ed74b87691f2ac72a8f7cddfc8
Sebastian <s3v4s61@gmail.com>
2021-03-24 22:24:25 -0600

    Se empezo añadir el cuerpo

--- Vistas/index.php
+++ Vistas/index.php
@@ -9,6 +9,18 @@ include("./Vistas/components/header.php");
 include("./Vistas/components/navegador.html");
 ?>
 
+<div class="container">
+    <div class="row row-cols-3">
+        <div class="col-3 border border-info"> 
+            <h3>Information</h3>
+        </div>
+        <div class="col-6"><
+                 
+        </div>
+        <div class="col-3"><h1>Derecha</h1></div>
+    </div>
+
+</div>
 
 
 
