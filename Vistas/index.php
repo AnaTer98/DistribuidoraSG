94a2ddaf534534c3375b05c21873c29c078af855
Sebastian <s3v4s61@gmail.com>
2021-03-24 23:34:55 -0600

    Modificacion del carrucel

--- Vistas/index.php
+++ Vistas/index.php
@@ -10,8 +10,8 @@ include("./Vistas/components/navegador.html");
 ?>
 
 <!--Carrusel-->
-<div class=" bg-primary  mx-auto" style="width:95%;margin-top: -2em;background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);">
-    <img src="./Vistas/images/01.png" class="img-fluid mx-auto d-block" alt="...">
+<div class=" bg-primary  mx-auto" style="width:80%;padding-bottom: 1em;padding-top: 1em;   margin-top: -2em;background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);">
+    <img src="./Vistas/images/01.png" class="img-fluid mx-auto d-block" alt="Prueba jajaj!">
 </div>
 <!--Contenido-->
 <div class="container">
