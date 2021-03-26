874a60ec85fb4a29da39f05d89a5c516ae78d27d
Sebastian <s3v4s61@gmail.com>
2021-03-25 19:18:37 -0600

    Correccion

--- database.php
+++ database.php
@@ -1,14 +1,10 @@
-<$php
-#Aqui sera la direccion de la base de datos
-#Usuario de la base de datos
-#Password
-#Nombre de la base de datos 
-    $conexion = mysqli_connect(
+<?php
+
+
+    $conn = mysqli_connect(
         'localhost', 
         'root',  
         '',   
         'electronicos' 
     );
-
-
 ?>
