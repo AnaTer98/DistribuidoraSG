44298352070e416d5e3dbb504d94a96e6ed25b57
Sebastian <s3v4s61@gmail.com>
2021-03-29 23:18:35 -0600

    NOmas di fromato jaja

--- database.php
+++ database.php
@@ -1,21 +1,17 @@
 <?php
- include('datosConexion.php');
-    #Se ara cambio de calse para la conexion a la base de datos 
-    class Database{
-       
-        function conexion(){
-            try{
-           $conn = new PDO("mysql:host=".SERVER.";dbname=".DBNAME,USER,PASSWORD);     
-           return $conn;
-            }catch(PDOException $error){
-                die("El error es :".$error->getMessage());
+include('datosConexion.php');
+//Se  cambio de clase para la conexion a la base de datos 
+class Database
+{
 
-            }
-
-
+    function conexion()
+    {
+        try {
+            $conn = new PDO("mysql:host=" . SERVER . ";dbname=" . DBNAME, USER, PASSWORD);
+            return $conn;
+        } catch (PDOException $error) {
+            die("El error es :" . $error->getMessage());
         }
-
-
-        
     }
+}
 ?>
