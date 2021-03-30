d789e10c342e93b5f792711985c76ee1b94c7826
Sebastian <s3v4s61@gmail.com>
2021-03-29 23:00:50 -0600

    configuracion de pdo

---
+++ datosConexion.php
@@ -0,0 +1,8 @@
+<?php
+    #Datos para realizar  la conexion a la base de datos
+    define('SERVER','localhost');
+    define('DBNAME','crud');#queda por fefinir 
+    define('USER','root');
+    define('PASSWORD','');
+
+?>
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-include("./database.php");
 
 
 include('./Vistas/index.php');
