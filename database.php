1c5931ae29e366cb2bcda8124c48b038f1503981
Sebastian <s3v4s61@gmail.com>
2021-03-29 22:59:58 -0600

    Nueva implementacion de ha pdo en ves de mysqli

--- database.php
+++ database.php
@@ -1,10 +1,21 @@
 <?php
+ include('datosConexion.php');
+    #Se ara cambio de calse para la conexion a la base de datos 
+    class Database{
+       
+        function conexion(){
+            try{
+           $conn = new PDO("mysql:host=".SERVER.";dbname=".DBNAME,USER,PASSWORD);     
+           return $conn;
+            }catch(PDOException $error){
+                die("El error es :".$error->getMessage());
+
+            }
 
 
-    $conn = mysqli_connect(
-        'localhost', 
-        'root',  
-        '',   
-        'electronicos' 
-    );
+        }
+
+
+        
+    }
 ?>
