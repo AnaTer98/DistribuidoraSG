e201a3246cb47834e3396fb9c7d98ba2cb8ab8e2
Sebastian <s3v4s61@gmail.com>
2021-04-23 21:49:19 -0500

    Cambio de estructura

--- views/index.php
+++ views/index.php
@@ -4,10 +4,6 @@ include("./views/components/header.html");
 
 
 //<!--Carrusel-->
-
-include("./views/components/navegador.html");
-
-
 include('./views/components/carrusel.html');
 
 
@@ -15,47 +11,49 @@ include('./views/components/carrusel.html');
 
 ?>
 
-<!--Contenido-->
-<div class=" container bg-light" id="contenido-bajo">
-    <div class="row " >
-    <div class="col-12">
-        <h1>Contenido::</h1>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-    
-    </div>
-    </div>
+<div class="d-flex" id="wrapper">
+
+<!-- Sidebar -->
+<div class="bg-light border-right" id="sidebar-wrapper">
+  <div class="sidebar-heading">Nuestros productos y servicios</div>
+  <div class="list-group list-group-flush">
+    <a href="#" class="list-group-item list-group-item-action bg-light">Plataforma de Recargas</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Accesorios</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Telefonía</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Cómputo</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Servicios</a>
+    <a href="#" class="list-group-item list-group-item-action bg-light">Novedades</a>
+  </div>
+</div>
+<!-- /#sidebar-wrapper -->
+
+<!-- Page Content -->
+<div id="page-content-wrapper"class="bg-light">
+<!--Aqui importar el navegador -->
+  <?php include('views/components/navegador.html');?>
+  <!--El botton para mostrar el menu lateral-->
+
+  <button class="btn btn-warning my-2 my-sm-0" id="menu-toggle" type="submit"><i class="bi bi-bar-chart-steps"></i></button>
+
+     
+
+
+  <div class="container-fluid ">
+    <h1 class="mt-4">Publicaciones</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laborum eius, debitis voluptatum at possimus saepe! Iure officiis tempore placeat illo exercitationem esse, impedit perferendis recusandae maiores laudantium vel et veniam accusantium sed optio modi quisquam accusamus delectus nesciunt eveniet corporis magnam qui itaque mollitia. Consectetur temporibus enim libero mollitia magnam veniam, nobis architecto voluptatem eos eum, delectus quod quas, molestiae provident. Ullam ex atque magnam, dolor blanditiis, expedita dignissimos dicta modi repellat et quae excepturi fugit est odit adipisci consequatur error eaque quam ducimus perferendis voluptas enim porro, delectus nesciunt. Dignissimos blanditiis dolorem nisi, officiis fugit quisquam cupiditate ipsam.</p> 
+  </div>
+</div>
+<!-- /#page-content-wrapper -->
 
 </div>
-
 <?php
-  include("./views/components/footer.html");
-
-  
-    include('./database.php');
-        $objeto = new Database();
-        $enlace = $objeto->conexion();
-
-        $consulta = "SELECT * FROM task";
-        $resultado = $enlace->prepare($consulta);
-        $resultado->execute();
-        $datos = $resultado->fetchAll();
-
-        var_dump($datos);
-        foreach($datos as $dat){
-            echo $dat['title']."<br>";
-        }
-
-        
-
-  
-   
+include('./views/components/footer.html');
 ?>
+ <script>
+    $("#menu-toggle").click(function(e) {
+      e.preventDefault();
+      $("#wrapper").toggleClass("toggled");
+    });
+  </script> 
+
+
