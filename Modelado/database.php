65f2240cd8810b0fcc1d46b83323fa969f4a9319
Sebastian <s3v4s61@gmail.com>
2021-04-30 20:48:42 -0500

    Modificacion de la clase para la conexion

--- Modelado/database.php
+++ Modelado/database.php
@@ -1,14 +1,29 @@
 <?php
-include('datosConexion.php');
-//Se  cambio de clase para la conexion a la base de datos 
-class Database
-{
 
-    function conexion()
-    {
+//Se  cambio de clase para la conexion a la base de datos 
+class database{
+   #Datos para realizar  la conexion a la base de datos
+ private $host;
+ private $user;
+ private $password;
+ private $dbName;
+
+    public function __construct(){
+        $this->host = 'localhost';
+        $this->user = 'root';
+        $this->password ='';
+        $this->dbName = 'distribuidora';
+
+
+    }
+
+    function conexion(){
         try {
-            $conn = new PDO("mysql:host=" . SERVER . ";dbname=" . DBNAME, USER, PASSWORD);
+            $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_EMULATE_PREPARES => false];
+            $conn = new PDO("mysql:host=" . $this->host. ";dbname=" . $this->dbName,$this->user, $this->password,$options);
+
             return $conn;
+        
         } catch (PDOException $error) {
             die("El error es :" . $error->getMessage());
         }
