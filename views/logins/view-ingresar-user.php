6a03db8abb1860eafeb74fd104487cf326509bfc
sebastian <s3v4s61@gmail.com>
2021-05-30 14:26:02 -0500

    Cambio de nombre y vista para ingresar usuario

---
+++ views/logins/view-ingresar-user.php
@@ -0,0 +1,54 @@
+<?php
+
+include('../components/header.html');
+
+//include('../components/navegador.html');
+
+
+?>
+<div class="container row-col-12 bg-light" style="height: 35rem;" id="form-ingresar">
+  <div class="pt-4">
+<div class="card text-white border border-success mx-auto " id="card-inresar-user" style="max-width: 20rem;">
+  <div class="card-header bg-success">Ingresa</div>
+  <div class="card-body text-dark">
+    <form id="form-ingresar-user" class="login-form validate-form">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">Ingresa un correo ya registrado(somos una empresa seria no compartiremos tu información) </small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <a href="">¿No te has registrado?</a>
+      <br>
+           <button type="submit" name="ingresar" class="btn btn-primary">Entrar</button>
+    </form> 
+  </div>
+</div>
+</div>
+</div>
+
+
+<?php
+include('../components/footer.html');
+?>
+
+<script>
+
+  const stylesCss = document.getElementById('stylesCSS');
+  stylesCss.setAttribute('href','../index.css');
+
+
+ document.addEventListener('submit', function(e){
+   e.preventDefault();
+  Swal.fire(
+  'Llena todo!',
+  'You clicked the button!',
+  'success'
+)
+});
+
+
+</script>
---
+++ views/logins/view-new-login.php
@@ -0,0 +1,36 @@
+
+  <?php
+    include('../components/header.html');
+   
+  ?>
+
+
+
+
+<script>
+
+  const stylesCss = document.getElementById('stylesCSS');
+  stylesCss.setAttribute('href','../index.css');
+
+ 
+</script>
+<script>
+const nombre = document.getElementById('inputNombre');
+const correo = document.getElementById('inputCorreo');
+const pass = document.getElementById('inputContrasena');
+
+</script>
+
+
+<?php
+  include('../components/footer.html');
+?>
+
+
+<!--<script src="./login-ajax.js"></script>-->
+
+
+
+
+
+
