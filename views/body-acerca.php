c862216e1ac3b1c556a373d55d4bc16163ff1a17
sebastian <s3v4s61@gmail.com>
2021-06-06 23:47:45 -0500

    modificacion de las rutas

---
+++ views/body-acerca.php
@@ -0,0 +1,20 @@
+
+  <?php
+    include('./components/header.html'); 
+    include('./components/navegador.php'); 
+  ?>
+
+    
+
+      <script>
+        const logo=document.getElementById('logo-nav');
+        logo.setAttribute('src','./images/logo.png');
+
+        const stylesCss = document.getElementById('stylesCSS');
+  stylesCss.setAttribute('href','./index.css');
+      </script>
+
+
+  <?php
+    include('./components/footer.html');
+  ?>
