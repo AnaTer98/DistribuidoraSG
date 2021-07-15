ef13dbc7552b714b79c2c721676b7cb9ce242dfb
Tere Barona <anter.var98@gmail.com>
2021-07-15 14:31:28 -0500

    sepa que le movi jsjsjs

---
+++ views/Interacciones/vacantes.php
@@ -0,0 +1,23 @@
+<?php
+include("../components/header.html");
+?>
+
+
+
+
+
+
+
+
+
+<script>
+const logo= document.getElementById('logo-nav');
+  logo.setAttribute('src','./images/logo.png');
+  
+  const stylesCss = document.getElementById('stylesCSS');
+  stylesCss.setAttribute('href','./index.css');
+</script>
+<?php
+    include('../views/components/footer.html');
+
+?>
