b2bb4f211ae32edfbe80c67320ebf697f60522f9
sebastian <s3v4s61@gmail.com>
2021-07-15 14:11:49 -0500

    Preparativos

--- views/index.php
+++ views/index.php
@@ -34,17 +34,30 @@ include('./views/components/carrusel.html');
 <!--Aqui importar el navegador -->
   <?php include('views/components/navegador.php');?>
   <!--El botton para mostrar el menu lateral-->
-
   <button class="btn btn-warning my-2 my-sm-0" id="menu-toggle" type="submit"><i class="bi bi-bar-chart-steps"></i></button>
 
-     
+  <div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="..." alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>     
 
 
   <div class="container-fluid ">
     <h1 class="mt-4">Publicaciones</h1>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore laborum eius, debitis voluptatum at possimus saepe! Iure officiis tempore placeat illo exercitationem esse, impedit perferendis recusandae maiores laudantium vel et veniam accusantium sed optio modi quisquam accusamus delectus nesciunt eveniet corporis magnam qui itaque mollitia. Consectetur temporibus enim libero mollitia magnam veniam, nobis architecto voluptatem eos eum, delectus quod quas, molestiae provident. Ullam ex atque magnam, dolor blanditiis, expedita dignissimos dicta modi repellat et quae excepturi fugit est odit adipisci consequatur error eaque quam ducimus perferendis voluptas enim porro, delectus nesciunt. Dignissimos blanditiis dolorem nisi, officiis fugit quisquam cupiditate ipsam.</p> 
+  
   </div>
 </div>
+
 <!-- /#page-content-wrapper -->
 
 </div>
