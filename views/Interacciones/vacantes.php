c33c899de3da39d5df552daed04e9f99025e952d
Tere Barona <anter.var98@gmail.com>
2021-07-15 17:30:05 -0500

    aver

--- views/Interacciones/vacantes.php
+++ views/Interacciones/vacantes.php
@@ -3,31 +3,6 @@ include("../components/header.html");
 include("../components/navegador.php")
 ?>
 
-
-
-<div class="card mx-auto "style="max-width: 20rem;">
-<form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Nombre de la vacante</label>
-    <input type="" class="form-control" id="nombrevacante">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Description</label>
-    <textarea class="form-control" id="description" rows="3"></textarea>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Requeriments</label>
-    <textarea class="form-control" id="requeriments" rows="3"></textarea>
-  </div>
-</form>
-
-</div>
-
-
-
-
-
-<!--Otro-->
 <div class="row col-10 mx-auto bg-light">
 <form class="mx-auto p-4 col-11">
   <div class="form-group">
