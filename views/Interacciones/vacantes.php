cd13400f332425241a8a2af6594c11dd8162256f
sebastian <s3v4s61@gmail.com>
2021-07-15 14:37:10 -0500

    Cambio de rutas

--- views/Interacciones/vacantes.php
+++ views/Interacciones/vacantes.php
@@ -1,23 +1,26 @@
 <?php
 include("../components/header.html");
+include("../components/navegador.php")
 ?>
 
 
 
 
 
+<h1>assasasa</h1>
 
 
 
+
+<?php
+    include('../components/footer.html');
+
+?>
 
 <script>
 const logo= document.getElementById('logo-nav');
-  logo.setAttribute('src','./images/logo.png');
+  logo.setAttribute('src','../images/logo.png');
   
   const stylesCss = document.getElementById('stylesCSS');
-  stylesCss.setAttribute('href','./index.css');
+  stylesCss.setAttribute('href','../index.css');
 </script>
-<?php
-    include('../views/components/footer.html');
-
-?>
