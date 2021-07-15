e7d786dddfac1f9b7962070f95e7d4655cd67955
sebastian <s3v4s61@gmail.com>
2021-07-15 17:11:30 -0500

    Cambio de estilos en formulario

--- views/Interacciones/vacantes.php
+++ views/Interacciones/vacantes.php
@@ -4,6 +4,7 @@ include("../components/navegador.php")
 ?>
 
 
+<div class="card mx-auto "style="max-width: 20rem;">
 <form>
   <div class="form-group">
     <label for="exampleFormControlInput1">Nombre de la vacante</label>
@@ -19,8 +20,28 @@ include("../components/navegador.php")
   </div>
 </form>
 
+</div>
 
 
+<!--Otro-->
+<div class="row col-10 mx-auto bg-light">
+<form class="mx-auto p-4 col-11">
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Nombre de la vacante</label>
+    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Vendedor">
+  </div>
+  
+  
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Description</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Requeriments</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
+</div>
 
 <?php
     include('../components/footer.html');
