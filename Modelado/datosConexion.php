998eec69aa0c8a6a37652c7a7d015d95b743d26e
7sevas7 <s3v4s61@gmail.com>
2021-08-07 22:24:39 -0500

    Eliminar erchivo

--- Modelado/datosConexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    #Datos para realizar  la conexion a la base de datos
-    define('SERVER','localhost');
-    define('DBNAME','crud');#queda por fefinir 
-    define('USER','root');
-    define('PASSWORD','');
-
-?>
