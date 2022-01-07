b7fe716ec877c192c70de9618588368ca6827062
sebastian <s3v4s61@gmail.com>
2022-01-06 19:43:03 -0600

    Cambio el nombre

---
+++ views/vacantes.php
@@ -0,0 +1,45 @@
+<?php
+include "views/components/header.php";
+include "views/components/navegador.php";
+?>
+<div class="container col-11 bg-gray-300 rounded ">
+    <div class="row col-3 justify-content-mx-center">
+        <div class="col">
+        <div class="card " style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        
+        <div class="col">
+        <div class="card " style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+
+        <div class="col">
+        <div class="card " style="width: 18rem;">
+            <img class="card-img-top" src="..." alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+
+        
+    </div>
+</div>
+<?php
+include "views/components/footer.php";
+?>
