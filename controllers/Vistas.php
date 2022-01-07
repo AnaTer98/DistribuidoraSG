74616a36798eef40f8c77b0499e41f5b757106ca
sebastian <s3v4s61@gmail.com>
2022-01-06 19:41:00 -0600

    Comentario de objetos

--- controllers/Vistas.php
+++ controllers/Vistas.php
@@ -5,10 +5,10 @@
         public  $model;
         function __construct(){
             require_once('./models/modeloInicio.php');
-            $this->model = new ModeloInicio();
+           # $this->model = new ModeloInicio();
         }
         public function index(){
-            $data['imagenesC']=$this->model->getCarrusel();   
+           # $data['imagenesC']=$this->model->getCarrusel();   
             
         require_once "views/inicio.php";
       
@@ -21,10 +21,12 @@
         public function catalogo(){}
         public function cotizar(){}        
         public function atencionPersonalizada(){}
+
         public function vacantes(){require_once "views/vacantes.php";}
         public function ingresarCv(){}
 
-
+        public function qsomos(){ require_once "views/static/body-acerca.php";
+        }
         public function registrar(){
      
             require_once 'views/logins/view-new-login.php'; 
